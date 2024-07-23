8cfad88df565ae57d44772ba7bad685a33505e49
Admin-github123 <patelsiddhant9297@gmail.com>
2024-07-23 18:40:01 +0530

    project structure

---
+++ src/test/java/com/doctor_appointment/DoctorAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctor_appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
